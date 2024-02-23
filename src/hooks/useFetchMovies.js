c04d800fb9211a4bff7fdafe279f986ca8bfe31d
luiseduardocastillovelasquez <149502552+luiseduardocastillovelasquez@users.noreply.github.com>
2024-02-23 17:16:04 -0500

    Update useFetchMovies.js

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -17,7 +17,6 @@ const useFetchMovies = (movieId, serviceType) => {
 
                 const response = await fetch(url, {
                     method: 'POST',
-                    mode: 'no-cors',
                     headers: {
                         'Content-Type': 'application/json',
                     },
