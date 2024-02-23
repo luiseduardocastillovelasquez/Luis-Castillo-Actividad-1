f4aef36ca69a0296584b6e42dcf302b10050a5f2
luiseduardocastillovelasquez <149502552+luiseduardocastillovelasquez@users.noreply.github.com>
2024-02-23 16:36:43 -0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,1 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-        '/ms-movie-store-search',
-        createProxyMiddleware({
-            target: 'https://back-end-cloud-gateway-filters-production.up.railway.app',
-            changeOrigin: true,
-        })
-    );
-};
